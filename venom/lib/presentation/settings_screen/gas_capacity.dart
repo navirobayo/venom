a2d8b2da67a4beb8e559b1f66df05b1d25f4efe1
Ivan Robayo <robayoivan2077@gmail.com>
2023-07-03 23:37:42 -0500

    Databases sync

--- venom/lib/presentation/settings_screen/gas_capacity.dart
+++ venom/lib/presentation/settings_screen/gas_capacity.dart
@@ -11,125 +11,137 @@ class GasCapacity extends StatefulWidget {
 }
 
 class _GasCapacityState extends State<GasCapacity> {
-  late Vehicle _gasVehicle;
+  late Future<Vehicle> _defaultVehicleFuture;
 
   @override
   void initState() {
     super.initState();
-    _getDefaultVehicle();
+    _defaultVehicleFuture = _getDefaultVehicle();
   }
 
-  Future<void> _getDefaultVehicle() async {
+  Future<Vehicle> _getDefaultVehicle() async {
     final defaultVehicleDatabase = DefaultVehicleDatabase.instance;
     final defaultVehicle = await defaultVehicleDatabase.defaultVehicle();
-    final gasVehicle = defaultVehicle != null
-        ? Vehicle(
-            name: defaultVehicle.vehicleName,
-            tankCapacity: defaultVehicle.vehicleTankSize,
-          )
-        : Vehicle(name: '---', tankCapacity: '---');
-    setState(() {
-      _gasVehicle = gasVehicle;
-    });
+    final vehicle = Vehicle(
+      name: defaultVehicle.vehicleName,
+      tankCapacity: defaultVehicle.vehicleTankSize,
+    );
+    return vehicle;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Gas Capacity"),
+        title: const Text("Default Vehicle Selector"),
       ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 25,
-          ),
-          Row(
-            children: [
-              const Spacer(),
-              Text(
-                _gasVehicle.tankCapacity,
-                style: const TextStyle(fontSize: 48.0),
-              ),
-              const Text("-GAL", style: TextStyle(fontSize: 48.0)),
-              const Spacer(),
-              Text(
-                _gasVehicle.name,
-              ),
-              const Spacer(),
-            ],
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final database = VehicleDatabase();
-              final vehicles = await database.vehicles();
-              if (vehicles.isEmpty) {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('No current vehicles'),
-                      content:
-                          const Text('Please add them in "My vehicles" screen'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              } else {
-                final selectedVehicle = await showDialog<Vehicle>(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Select a vehicle'),
-                      content: SizedBox(
-                        width: double.maxFinite,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: vehicles.length,
-                          itemBuilder: (context, index) {
-                            final vehicle = vehicles[index];
-                            return ListTile(
-                              title: Text(vehicle.name),
-                              onTap: () {
-                                Navigator.pop(context, vehicle);
-                              },
-                            );
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                );
-                if (selectedVehicle != null) {
-                  // Insert the selected vehicle as the new default vehicle
-                  final defaultVehicle = DefaultVehicleObject(
-                    vehicleName: selectedVehicle.name,
-                    vehicleTankSize: selectedVehicle.tankCapacity,
-                  );
-                  final defaultVehicleDatabase =
-                      DefaultVehicleDatabase.instance;
-                  await defaultVehicleDatabase
-                      .insertDefaultVehicle(defaultVehicle);
+      body: FutureBuilder<Vehicle>(
+        future: _defaultVehicleFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final defaultVehicle = snapshot.data!;
+            return ListView(
+              children: [
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  children: [
+                    const Spacer(),
+                    Text(
+                      defaultVehicle.tankCapacity,
+                      style: const TextStyle(fontSize: 48.0),
+                    ),
+                    const Text("-GAL", style: TextStyle(fontSize: 48.0)),
+                    const Spacer(),
+                    Text(
+                      defaultVehicle.name,
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final database = VehicleDatabase();
+                    final vehicles = await database.vehicles();
+                    if (vehicles.isEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('No current vehicles'),
+                            content: const Text(
+                                'Please add them in "My vehicles" screen'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      final selectedVehicle = await showDialog<Vehicle>(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Select a vehicle'),
+                            content: SizedBox(
+                              width: double.maxFinite,
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: vehicles.length,
+                                itemBuilder: (context, index) {
+                                  final vehicle = vehicles[index];
+                                  return ListTile(
+                                    title: Text(vehicle.name),
+                                    onTap: () {
+                                      Navigator.pop(context, vehicle);
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                      if (selectedVehicle != null) {
+                        // Insert the selected vehicle as the new default vehicle
+                        final defaultVehicle = DefaultVehicleObject(
+                          vehicleName: selectedVehicle.name,
+                          vehicleTankSize: selectedVehicle.tankCapacity,
+                        );
+                        final defaultVehicleDatabase =
+                            DefaultVehicleDatabase.instance;
+                        await defaultVehicleDatabase
+                            .insertDefaultVehicle(defaultVehicle);
 
-                  setState(() {
-                    _gasVehicle = selectedVehicle;
-                  });
-                }
-              }
-            },
-            child: const Text('Select vehicle'),
-          ),
-        ],
+                        setState(() {
+                          _defaultVehicleFuture = Future.value(selectedVehicle);
+                        });
+                      }
+                    }
+                  },
+                  child: const Text('Select vehicle'),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("Error"),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
